19047c3550617b8841b1094dfcad0d8d4cd590e5
Abdullah Khetran <khetranabdullah@gmail.com>
2026-05-05 16:58:40 +0500

    added localhost ip address to whitelist

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import {Server} from "socket.io"
 const httpServer = createServer()
 const io = new Server(httpServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 })
 
